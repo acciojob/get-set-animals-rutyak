0f06f8c8b21daa19cbbb2eb39146c85eb34125c7
rutyak <132219103+rutyak@users.noreply.github.com>
2023-08-14 14:12:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 class Animal {
   constructor(species) {
-    this.species = species;
+    this._species = species;
   }
 
+  get species(){
+	  return this._species;
+  }
+	
   makeSound() {
     console.log("Animal sound");
   }
@@ -12,20 +16,35 @@ class Cat extends Animal {
   purr() {
     console.log("purr");
   }
+
+   makeSound() {
+    console.log("Meow");
+  }
 }
 
 class Dog extends Animal {
   bark() {
     console.log("woof");
   }
+
+   makeSound() {
+    console.log("Auu Auu Auu");
+  }
 }
 
-const dog = new Dog();
-const cat = new Cat();
+const ani = new Animal("wild Animal");
+console.log(ani.species); // wild aminal
+ani.makeSound(); // Animal sound
 
-dog.bark();
+const cat = new Cat("Cat");
+console.log(cat.species); //cat
+cat.makeSound(); // Meow
 cat.purr();
-cat.makeSound();
+
+const dog = new Dog("Dog");
+console.log(dog.species); //dog
+dog.makeSound(); // Auu Auu Auu
+dog.bark();
 
 // Do not change the code below this line
 window.Animal = Animal;
